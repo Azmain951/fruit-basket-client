808d64f3893613ba9a86d54407dc8125082fdc55
Afridi Azmain <Azmain951>
2022-05-08 14:32:18 +0600

    footer design updated

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className='footer bg-dark text-light d-flex justify-content-between p-4 mt-3'>
             <p>&copy; Copyright {year}, All Rights Reserved.</p>
-            <p>Website Developed By: Afridi Azmain</p>
+            <p>Website Developed By <span className='fw-bold'>Afridi Azmain</span></p>
         </div>
     );
 };
